b13f48a7e99bfc446e56fdd9f5b89caa11c514ef
Jeevan Ramakrishna <jeevan.kr@gmail.com>
2021-12-21 12:25:14 -0800

    adding compare two lists

--- samples.groovy
+++ samples.groovy
@@ -14,10 +14,23 @@ def createEscapedJson() {
     // jsonObj is escaped via creating another json object - stringify
     println(JsonOutput.toJson(jsonObj))
 }
-
+// extracts the common elements from lists and returns a list of common elements
+// excludes nulls
+def getCommonElements(firstList, secondList) {
+    def opt = []
+    for (f in firstList) {
+        for (s in secondList) {
+            if (f != null && f.equals(s)) {
+                opt.add(s)
+            }
+        }
+    }
+    return opt
+}
 def main() {
     hello_world()
     createEscapedJson()
+    println(getCommonElements(["dog", "cat", "mouse", "racoon"], ["elephant", "mouse", "cat"]))
 }
 
 main()
