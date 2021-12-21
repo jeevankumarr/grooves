55fac59db7eae75567baee126dd630e15965cc9b
Jeevan Ramakrishna <jeevan.kr@gmail.com>
2021-12-21 11:03:51 -0800

    seperated functions

--- samples.groovy
+++ samples.groovy
@@ -3,9 +3,11 @@ import groovy.json.JsonOutput
 
 def hello_world() {
     println("Hello, World!")
+}
+
+def createEscapedJson() {
     def habits = "coffee ps4 games"
     def jsonObj = JsonOutput.toJson([name: "Jeevan", habits: habits.split(" ")])
     println(JsonOutput.toJson(jsonObj))
 }
-
 hello_world()
